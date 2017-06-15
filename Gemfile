ab969ad2b8937eece090c22c83f74fcdb9de19d3
Oleg Mokhnyuk <olegmokhnyuk@gmail.com>
2017-06-15 23:12:19 +0200

    Create init project
    Define base structure, add layout, styles placeholders, helpers, gems

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+# HTML Minify
+gem 'middleman-minify-html'
+
+# Favicon maker: https://github.com/follmann/middleman-favicon-maker
+gem 'middleman-favicon-maker', '~> 4.0'
+
+# Google Analytics: https://github.com/danielbayerlein/middleman-google-analytics
+gem 'middleman-google-analytics', '~> 3.0'
+
+# Image optimization
+gem "middleman-imageoptim", :git => "https://github.com/plasticine/middleman-imageoptim", :branch => "master"
---
+++ config.rb
@@ -0,0 +1,119 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :data_dir, "data"
+set :layout, "layouts/layout"
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+module WebsiteHelpers
+	# updates year in copyright for you
+	def copyright_year_range(start_year)
+		# https://rietta.com/blog/2011/12/26/how-to-automate-copyright-notice/
+
+	  start_year = start_year.to_i
+	  current_year = Time.new.year
+
+	  if current_year > start_year && start_year > 2010
+	    "#{start_year}–#{current_year}"
+	  elsif start_year > 2010
+	    "#{start_year}"
+	  else
+	    "#{current_year}"
+	  end
+	end
+
+
+	# adds class "active" to current page
+	def nav_link(link_text, page_url, options = {})
+    options[:class] ||= ""
+
+    if current_page.url.length > 1
+      current_url = current_page.url.chop
+    else
+      current_url = current_page.url
+    end
+    options[:class] << " active" if page_url == current_url
+    link_to(link_text, page_url, options)
+	 end
+end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+configure :build do
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = true   # Remove inter-tag spaces
+    html.remove_quotes              = true   # Remove quotes
+    html.simple_doctype             = true   # Use simple doctype
+    html.remove_script_attributes   = true   # Remove script attributes
+    html.remove_style_attributes    = true   # Remove style attributes
+    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = true   # Remove input attributes
+    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_http_protocol       = false  # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    html.preserve_patterns          = nil    # Patterns to preserve
+  end
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
+  activate :imageoptim
+  activate :favicon_maker do |f|
+    f.template_dir  = 'source/images'
+    f.icons = {
+      "_favicon_template.png" => [
+        { icon: "apple-touch-icon-180x180-precomposed.png" },
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "favicon-160x160.png" },
+        { icon: "mstile-144x144", format: :png },
+      ],
+      "_favicon_template_lores.png" => [
+        { icon: "favicon-32x32.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      ]
+    }
+  end
+end
